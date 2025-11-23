c4f439dd77cbdc5c5a78ff181f9127b787eb2464
rhy3h <rhy3htw@gmail.com>
2025-11-23 15:56:54 +0800

    refactor: rename

--- forge.config.ts
+++ forge.config.ts
@@ -25,12 +25,12 @@ const config: ForgeConfig = {
       build: [
         {
           // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
-          entry: 'src/main.ts',
+          entry: 'src/electron/main.ts',
           config: 'vite/vite.main.config.ts',
           target: 'main',
         },
         {
-          entry: 'src/preload.ts',
+          entry: 'src/electron/preload.ts',
           config: 'vite/vite.preload.config.ts',
           target: 'preload',
         },
